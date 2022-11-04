b726409d62082dc6d89e2f3b8a1e09089d8b18f2
clc <c.leclercq.ext@fareco.fayat.com>
2022-11-04 15:04:09 +0100

    Copy ssl key and cert to remoteJournal

--- src/libyocto/config/config_ssl.c
+++ src/libyocto/config/config_ssl.c
@@ -13,27 +13,33 @@
 #define CONFIG_SSL_KEY_MINSIZE 1024
 #define CONFIG_SSL_KEY_MAXSIZE 4096
 
-//From: meta-fareco/recipes-extended/lighttpd/lighttpd/lighttpd.conf
-//From: meta-fareco/recipes-support/stunnel/stunnel/stunnel.conf
-#define CONFIG_SSL_FILENAME_KEY "/etc/security/ssl/private/ssl.key.pem"
-#define CONFIG_SSL_FILENAME_CERT "/etc/security/ssl/certs/ssl.cert.pem"
+// From: meta-fareco/recipes-extended/lighttpd/lighttpd/lighttpd.conf
+// From: meta-fareco/recipes-support/stunnel/stunnel/stunnel.conf
+#define CONFIG_SSL_KEY_FILENAME "/etc/security/ssl/private/ssl.key.pem"
+#define CONFIG_SSL_CERT_FILENAME "/etc/security/ssl/certs/ssl.cert.pem"
+
+#define CONFIG_SSL_REMOTE_JOURNAL_KEY_FILENAME "/etc/security/journal-upload/private/ssl.key.pem"
+#define CONFIG_SSL_REMOTE_JOURNAL_CERT_FILENAME "/etc/security/journal-upload/certs/ssl.cert.pem"
 
 int configSslWrite(const char *privateKey, const char *certificate)
 {
     assert(privateKey);
     assert(certificate);
 
+    // FIXME COPY to journal also
     if (
         configCheckSize(privateKey, CONFIG_SSL_KEY_MINSIZE, CONFIG_SSL_KEY_MAXSIZE) < 0 ||
         configCheckSize(certificate, CONFIG_SSL_KEY_MINSIZE, CONFIG_SSL_KEY_MAXSIZE) < 0)
     {
-
         return -1;
     }
 
-   if (
-        configWriteFile(CONFIG_SSL_FILENAME_KEY, privateKey) < 0 ||
-        configWriteFile(CONFIG_SSL_FILENAME_CERT, certificate) < 0)
+    //Copy key and cert to ssl and remoteJournal folders (owner permissions)
+    if (
+        configWriteFile(CONFIG_SSL_KEY_FILENAME, privateKey) < 0 ||
+        configWriteFile(CONFIG_SSL_CERT_FILENAME, certificate) < 0 ||
+        configWriteFile(CONFIG_SSL_REMOTE_JOURNAL_KEY_FILENAME, privateKey) < 0 ||
+        configWriteFile(CONFIG_SSL_REMOTE_JOURNAL_CERT_FILENAME, certificate) < 0)
     {
         return -1;
     }
