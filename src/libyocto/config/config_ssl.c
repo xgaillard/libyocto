6e0a755000a0bad96df7e387c439512311c9de59
clc <c.leclercq.ext@fareco.fayat.com>
2023-03-06 09:59:21 +0100

    Added comments to config_ssl

--- src/libyocto/config/config_ssl.c
+++ src/libyocto/config/config_ssl.c
@@ -9,7 +9,7 @@
 #include <errno.h>
 #include <assert.h>
 
-// TODO Compute sensible values
+//TODO Compute sensible values
 #define CONFIG_SSL_KEY_MINSIZE 1024
 #define CONFIG_SSL_KEY_MAXSIZE 4096
 
@@ -18,6 +18,7 @@
 #define CONFIG_SSL_KEY_FILENAME "/etc/security/ssl/private/ssl.key.pem"
 #define CONFIG_SSL_CERT_FILENAME "/etc/security/ssl/certs/ssl.cert.pem"
 
+// From: meta-fareco/recipes-core/systemd/systemd-conf/journal-upload.conf.d/20-default.conf
 #define CONFIG_SSL_REMOTE_JOURNAL_KEY_FILENAME "/etc/security/journal-upload/private/ssl.key.pem"
 #define CONFIG_SSL_REMOTE_JOURNAL_CERT_FILENAME "/etc/security/journal-upload/certs/ssl.cert.pem"
 
@@ -26,7 +27,6 @@ int configSslWrite(const char *privateKey, const char *certificate)
     assert(privateKey);
     assert(certificate);
 
-    // FIXME COPY to journal also
     if (
         configCheckSize(privateKey, CONFIG_SSL_KEY_MINSIZE, CONFIG_SSL_KEY_MAXSIZE) < 0 ||
         configCheckSize(certificate, CONFIG_SSL_KEY_MINSIZE, CONFIG_SSL_KEY_MAXSIZE) < 0)
@@ -34,7 +34,8 @@ int configSslWrite(const char *privateKey, const char *certificate)
         return -1;
     }
 
-    //Copy key and cert to ssl and remoteJournal folders (owner permissions)
+    // Copy key and cert to ssl and remoteJournal folders (owner permissions)
+    //FIXME chmod/fchmodat systemd-journal
     if (
         configWriteFile(CONFIG_SSL_KEY_FILENAME, privateKey) < 0 ||
         configWriteFile(CONFIG_SSL_CERT_FILENAME, certificate) < 0 ||
